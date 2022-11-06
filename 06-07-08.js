8aa492151c71be47e800a25d583c4c7cf6c01a8f
carlosvizcaino <116604158+carvizsophi@users.noreply.github.com>
2022-11-06 11:05:32 -0500

    Add files via upload
    challengel de examen final

---
+++ 06-07-08.js
@@ -0,0 +1,105 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function crearClaseViajero() {
+  class Viajero {
+    constructor(nombre = '', edad = 0, paises = [''], compañeros = [{}]) {
+      // El constructor de la clase Viajero recibe nombre (string), edad (integer), paises (array de strings), compañeros (array de objetos)
+      // Inicializar las propiedades del viajero con los valores recibidos como argumento
+
+      // Tu código aca:
+      this.nombre = nombre
+      this.edad = edad
+      this.paises = paises
+      this.compañeros = compañeros
+    }
+
+    addCompañero(nombre ='', nacionalidad ='', edad = 0) {
+      // El método 'addCompañero' recibe un string 'nombre', un string 'nacionalidad' y un entero 'edad' y
+      // debe agregar un objeto:
+      // { nombre: nombre, nacionalidad: nacionalidad, edad: edad} al arreglo de compañeros del viajero.
+      // No debe retornar nada.
+
+      // Tu código aca:
+      this.compañeros.push({nombre : nombre, nacionalidad, edad})
+
+    }
+
+    addPais(pais = '') {
+      // El método 'AddPais' recibe un string 'pais' y debe agregarlo al arreglo de paises del viajero.
+      // No debe retornar nada.
+
+      // Tu código aca:
+      this.paises.push(pais)
+    }
+
+    getCompañeros() {
+      // El método 'getCompañeros' debe retornar un arreglo con sólo los nombres del arreglo de compañeros
+      // del viajero.
+      // Ej:
+      // Suponiendo que el viajero tiene estos compañeros: [{nombre: 'John', nacionalidad: "Australiano", edad: 27},{nombre: 'Peter', nacionalidad: "Belga", edad: 23}]
+      // viajero.getCompañeros() debería devolver ['John', 'Peter']
+
+      // Tu código aca:
+      const nuevoArray = []
+      for(var compa of this.compañeros){
+        nuevoArray.push(compa.nombre)
+      }
+      return nuevoArray
+    }
+
+    getPaises() {
+      // El método 'getPaises' debe retornar un arreglo con los paises del viajero
+      // Ej:
+      // viajero.getPaises() debe devolver ['Belgica', 'Estados Unidos', 'Islandia']
+
+      // Tu código aca:
+      return this.paises
+    }
+
+    getPromedioEdad() {
+      // El método 'getPromedioEdad' debe retornar el promedio de edad de los compañeros de un viajero.
+      // Ej:
+      // Si el viajero tuviera estos compañeros:
+      // {
+      //   compañeros: [{
+      //     nombre: 'John',
+      //     nacionalidad: "Australiano",
+      //     edad: 27,
+      //   }, {
+      //     nombre: 'Peter',
+      //     nacionalidad: "Belga",
+      //     edad: 23
+      //   }]
+      // }
+      // viajero.getPromedioEdad() debería devolver 25 ya que (27 + 23) / 2 = 25
+
+      // Tu código aca:
+      var sumarEdad = 0
+      for(let compa of this.compañeros){
+        sumarEdad += compa.edad
+      }
+      return sumarEdad/this.compañeros.length
+    }
+  };
+
+  return Viajero;
+}
+const Viajero = crearClaseViajero()
+const carlos = new Viajero(
+  'carlos', 26,
+  ['belgica', 'estadosUnidos', 'islandia'],
+  [
+    {nombre: 'jhon', nacionalidad: 'australiano', edad:27},
+    {nombre: 'peter', nacionalidad: 'belga', edad: 23},
+  ]
+)
+carlos.addCompañero('jose', 'colombia', 22)
+carlos.addPais('inglaterra')
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = crearClaseViajero
